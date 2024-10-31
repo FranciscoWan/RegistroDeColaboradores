04fb33d81ca5cf5b4bf0970b194f2eb551554c60
FranciscoWan <franciscowan@outlook.com>
2024-10-31 11:06:40 -0300

    Adicionado opção 4 e confirmação de dados
    Foi implementado uma quarta opção para sair do programa no menu inicial, e também uma confirmação para saber se os dados cadastrados foram inseridos corretamente, caso sim o arquivo se mantem salvo, caso não, ele é excluído e é retornado a página inicial de registro.

--- Registro Alunos EBAC.c
+++ Registro Alunos EBAC.c
@@ -5,58 +5,88 @@
 
 int registro() // Função responsável por cadastrar os usuários no sistema
 {
-	//Início da criação de variáveis/strings
-	char arquivo[40];
-	char cpf[40];
-	char nome[40];
-	char sobrenome[40];
-	char cargo[40];
-	//Final da criação de variáveis/strings
+	int laco=1;
+	for (laco=1;laco=1;)
+	{
+		//Início da criação de variáveis/strings
+		char arquivo[40];
+		char cpf[40];
+		char nome[40];
+		char sobrenome[40];
+		char cargo[40];
+		//Final da criação de variáveis/strings
+		
+		printf("\tVocê selecionou registro.\n\n\tDigite o cpf a ser cadastrado: ");  // Coletando a informação do usuário para definir o cpf
+		scanf("%s", cpf);  // "%s" serve para armazenar uma string
+		
+		strcpy(arquivo, cpf);  // strcpy() serve para copiar o valor de uma variável de string para outra variável
 	
-	printf("\tVocê selecionou registro.\n\n\tDigite o cpf a ser cadastrado: ");  // Coletando a informação do usuário para definir o cpf
-	scanf("%s", cpf);  // "%s" serve para armazenar uma string
+		FILE *file;  // FILE cria o arquivo
+		file = fopen(arquivo,"w"); // fopen abre o arquivo e "w" escreve os dados da variável.
+		fprintf(file,cpf);  // fprintf serve para salvar a variável.
+		fclose(file);  // fclose fecha o arquivo.
 	
-	strcpy(arquivo, cpf);  // strcpy() serve para copiar o valor de uma variável de string para outra variável
+		file = fopen(arquivo,"a");  // "a" server para atualizar o arquivo existente
+		fprintf(file, ",");
+		fclose(file);
 	
-	FILE *file;  // FILE cria o arquivo
-	file = fopen(arquivo,"w"); // fopen abre o arquivo e "w" escreve os dados da variável.
-	fprintf(file,cpf);  // fprintf serve para salvar a variável.
-	fclose(file);  // fclose fecha o arquivo.
+		printf("\tDigite o nome a ser cadastrado: ");
+		scanf("%s", nome);
 	
-	file = fopen(arquivo,"a");  // "a" server para atualizar o arquivo existente
-	fprintf(file, ",");
-	fclose(file);
+		file = fopen(arquivo,"a");
+		fprintf(file, nome);
+		fclose(file);
 	
-	printf("\tDigite o nome a ser cadastrado: ");
-	scanf("%s", nome);
+		file = fopen(arquivo, "a");  // "a" server para atualizar o arquivo existente
+		fprintf(file, ",");
+		fclose(file);
 	
-	file = fopen(arquivo,"a");
-	fprintf(file, nome);
-	fclose(file);
+		printf("\tDigite o sobrenome a ser cadastrado: ");
+		scanf("%s", sobrenome);
+		
+		file = fopen(arquivo, "a");
+		fprintf(file, sobrenome);
+		fclose(file);
 	
-	file = fopen(arquivo, "a");  // "a" server para atualizar o arquivo existente
-	fprintf(file, ",");
-	fclose(file);
+		file = fopen(arquivo, "a");  // "a" server para atualizar o arquivo existente
+		fprintf(file, ",");
+		fclose(file);
 	
-	printf("\tDigite o sobrenome a ser cadastrado: ");
-	scanf("%s", sobrenome);
+		printf("\tDigite o cargo da pessoa cadastrada: ");
+		scanf("%s", cargo);
 	
-	file = fopen(arquivo, "a");
-	fprintf(file, sobrenome);
-	fclose(file);
+		file = fopen(arquivo, "a");
+		fprintf(file, cargo);
+		fclose(file);
 	
-	file = fopen(arquivo, "a");  // "a" server para atualizar o arquivo existente
-	fprintf(file, ",");
-	fclose(file);
-	
-	printf("\tDigite o cargo da pessoa cadastrada: ");
-	scanf("%s", cargo);
-	system("pause"); //Pausa o programa até  seleção de uma nova opção pelo usuário
-	
-	file = fopen(arquivo, "a");
-	fprintf(file, cargo);
-	fclose(file);
-	
+		// Criação da variável que irá defiir se os dado serão salvos ou excluidos
+		int salvar=0;
+		
+		printf("\n\n\tOs dados cadastrados estão corretos?\n");
+		printf("\t1 - Sim, os dados estão corretos.\n");  //Opção 1 mantém os dados salvos
+		printf("\t2 - Não, escrever novamente os dados.\n");  // Opção 2 exclui os dados recém escritos e pede novamente para escrever os dados
+		printf("\tSelecione uma opção: ");
+		scanf("%d", &salvar);  // Define o valor da variável inteira salvar
+		if (salvar==1)
+		{
+			printf("\n\tOs dados foram salvados com sucesso.\n");
+			printf("\tRetornando ao menu inicial.\n\t");
+			system("pause");
+			break; // Sai do loop 
+		}
+		else if (salvar==2)
+		{
+			remove(arquivo);
+			printf("\n\tOs dados não foram salvos.\n\tReiniciando o registro.\n\t");
+			system("pause");
+			system("cls");
+		}
+		else
+		{
+			printf("\n\tOpção inválida, cadastro reiniciado.\n\n");
+			system("pause");
+		}
+	}
 }
 
 int consulta()
@@ -121,10 +151,11 @@ int main()  // Função principal, o progrma sempre retornará nesta função
 		printf("  Selecione a opção desejada: \n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Excluir nomes\n\n");
+		printf("\t3 - Excluir nomes\n");
+		printf("\t4 - Sair do programa\n\n");
 		printf("\tOpção: ");  // Final da informação do menu
 	
-		scanf("%d", &opcao);  //Seleção da opção por parte do usuário  // "%d" é utilizado para armazenar uma variável
+		scanf("%d", &opcao);  //Seleção da opção por parte do usuário  // "%d" é utilizado para armazenar uma variável de valor inteiro
 	
 		system("cls");  // Comando para limpar a tela de prompt
 		
@@ -140,6 +171,11 @@ int main()  // Função principal, o progrma sempre retornará nesta função
 			
 			case 3:
 			excluir();  // Chamado da função excluir();
+			break;
+			
+			case 4:
+			printf("\tObrigado por utilizar o sistema!\n"); 
+			return 0;  // Transforma o valor da variável 'laço' = 0 e quebra o loop do for, encerrando o programa
 			break;
 			
 			default:
